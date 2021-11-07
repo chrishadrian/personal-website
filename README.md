3c6a86d5475e54622dbbf7e8a75ed2f5f3ade9df
Christopher Hadrian Wong <65900990+chrishadrian@users.noreply.github.com>
2021-11-07 10:50:06 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# portfolio_website
+ Javascript Mastery Tutorial
---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+
+const Hero = (props) => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hello World! <br />
+        I am Christopher Wong
+      </SectionTitle>
+      <SectionText>
+      An independent and self-motivated sophomore computer science student with experience working on website development. Looking for a role where I can grow and learn from the experienced team members while implementing current skills on projects.
+      </SectionText>
+      <Button onClick={() => window.location = 'https://linkedin.com/in/chrishadrianwong'}>Learn More</Button>
+    </LeftSection>
+  </Section>
+);
+
+export default Hero;
