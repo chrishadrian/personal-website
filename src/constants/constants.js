527895b79aaf11600efe5cffe6a094018272a543
Christopher Hadrian Wong <65900990+chrishadrian@users.noreply.github.com>
2022-01-29 11:09:02 -0800

    Updated projects order to most-relevant ones

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,7 @@
 export const projects = [
   {
     title: 'Unixpense',
-    description: "Using Java, Java Swing, and JSON, I created an object-oriented graphical user interface application from scratch. The App is called Unixpense and it is a simple expense tracker app that allows users to track their expenses by creating, deleting, saving, loading and analyzing their expense(s).",
+    description: "Using Java, Java Swing, and JSON, I created an object-oriented graphical user interface application from end-to-end. The App is called Unixpense and it is a simple expense tracker app that allows users to track their expenses by creating, deleting, saving, loading and analyzing their expense(s).",
       image: '/images/1.png',
       tags: ['Java', 'Java Swing', 'JSON'],
     source: 'https://google.com',
@@ -9,22 +9,13 @@ export const projects = [
     id: 0,
   },
   {
-    title: 'Richelle Shop',
-    description:"Using Wix, a cloud-based web development services, I developed a full-stack website for a shoe retail online shop. The website is called Richelle Shop, as it resonates its brand and originality, and it allows users to browse through store's inventories, starting from its picture, size, color, description, price, and payment method.",
-    image: '/images/2.png',
-    tags: ['Wix'],
-    source: 'https://richelle.shop',
-    visit: 'https://richelle.shop',
-    id: 1,
-  },
-  {
     title: 'HacktheNorth 2021: Prografit',
     description: "Using HTML, CSS, Javascript, I was responsible to build a chrome extension to send an alert every 20 minutes to users. Users have the option to opt-out from being reminded when the time is still going. The submitted data will then be transferred to the webpage, which was built by my other teammates that will display users' statistics over the week. I did this project during HackTheNorth 2021, the largest hackathon in North America.",
-      image: '/images/3.png',
-      tags: ['HTML', 'CSS', 'Javascript'],
+    image: '/images/3.png',
+    tags: ['HTML', 'CSS', 'Javascript'],
     source: 'https://github.com/northmen-hackers',
     visit: 'https://devpost.com/software/prografit',
-    id: 2,
+    id: 1,
   },
   {
     title: 'To-do list',
@@ -33,6 +24,15 @@ export const projects = [
     tags: ['React'],
     source: 'https://github.com/chrishadrian/todo-react',
     visit: 'https://github.com/chrishadrian/todo-react',
+    id: 2,
+  },
+  {
+    title: 'Richelle Shop',
+    description:"Using Wix, a cloud-based web development services, I developed a full-stack website for a shoe retail online shop. The website is called Richelle Shop, as it resonates its brand and originality, and it allows users to browse through store's inventories, starting from its picture, size, color, description, price, and payment method.",
+    image: '/images/2.png',
+    tags: ['Wix'],
+    source: 'https://richelle.shop',
+    visit: 'https://richelle.shop',
     id: 3,
   },
 ];
@@ -49,5 +49,5 @@ export const data = [
   { number: 7, text: 'Open Source Projects'},
   { number: 1, text: 'Hackathon Participation', },
   { number: 2, text: 'Github Followers', },
-  { number: 1, text: 'Github Stars', }
+  { number: 2, text: 'Github Stars', }
 ];
