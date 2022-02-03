cd32e67797b5d5fb3de89c58b0d644e5186ad230
Christopher Hadrian Wong <65900990+chrishadrian@users.noreply.github.com>
2022-02-03 11:22:12 -0800

    Updated constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,6 +2,7 @@ export const projects = [
   {
     title: 'Unixpense',
     description: "The App is called Unixpense and it is a simple expense tracker app that allows users to track their expenses by creating, deleting, saving, loading and analyzing their expense(s). Using Java, Java Swing, and JSON, I created an object-oriented graphical user interface application from end-to-end. I designed the object-oriented program (OOP) with Java, read and write the app's data with JSON, and created the graphical user interface (GUI) with Java Swing. This was made when I was completing my Software Construction course in The University of British Columbia.",
+      image: '/images/3.png',
       image: '/images/1.png',
       tags: ['Java', 'Java Swing', 'JSON'],
     source: 'https://google.com',
