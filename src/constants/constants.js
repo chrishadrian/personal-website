38c3eeca45087815457a57a5833af3bad920adc2
Christopher Wong <chrishadrianwong@gmail.com>
2023-09-06 12:18:44 -0700

    Updated constants

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,44 +1,53 @@
 export const projects = [
 	{
+		title: "GISAU Website",
+		description:
+			"The App is called Unixpense and it is a simple expense tracker app that allows users to track their expenses by creating, deleting, saving, loading and analyzing their expense(s). Using Java, Java Swing, and JSON, I created an object-oriented graphical user interface application from end-to-end. I designed the object-oriented program (OOP) with Java, read and write the app's data with JSON, and created the graphical user interface (GUI) with Java Swing.",
+		image: "/images/GISAU.png",
+		tags: ["Java", "Java Swing", "JSON"],
+		source: "https://github.com/chrishadrian/Unixpense",
+		visit: "https://github.com/chrishadrian/Unixpense",
+		id: 4,
+	},
+	{
+		title: "Recipe Buddy",
+		description:
+			"The App is called Unixpense and it is a simple expense tracker app that allows users to track their expenses by creating, deleting, saving, loading and analyzing their expense(s). Using Java, Java Swing, and JSON, I created an object-oriented graphical user interface application from end-to-end. I designed the object-oriented program (OOP) with Java, read and write the app's data with JSON, and created the graphical user interface (GUI) with Java Swing.",
+		image: "/images/RecipeBuddy.png",
+		tags: ["Java", "Java Swing", "JSON"],
+		source: "https://github.com/chrishadrian/Unixpense",
+		visit: "https://github.com/chrishadrian/Unixpense",
+		id: 3,
+	},
+	{
+		title: "Sync-Up",
+		description:
+			"The App is called Unixpense and it is a simple expense tracker app that allows users to track their expenses by creating, deleting, saving, loading and analyzing their expense(s). Using Java, Java Swing, and JSON, I created an object-oriented graphical user interface application from end-to-end. I designed the object-oriented program (OOP) with Java, read and write the app's data with JSON, and created the graphical user interface (GUI) with Java Swing.",
+		image: "/images/SyncUp.png",
+		tags: ["Java", "Java Swing", "JSON"],
+		source: "https://github.com/chrishadrian/Unixpense",
+		visit: "https://github.com/chrishadrian/Unixpense",
+		id: 2,
+	},
+	{
 		title: "Unixpense",
 		description:
 			"The App is called Unixpense and it is a simple expense tracker app that allows users to track their expenses by creating, deleting, saving, loading and analyzing their expense(s). Using Java, Java Swing, and JSON, I created an object-oriented graphical user interface application from end-to-end. I designed the object-oriented program (OOP) with Java, read and write the app's data with JSON, and created the graphical user interface (GUI) with Java Swing.",
-		image: "/images/3.png",
 		image: "/images/1.png",
 		tags: ["Java", "Java Swing", "JSON"],
 		source: "https://github.com/chrishadrian/Unixpense",
 		visit: "https://github.com/chrishadrian/Unixpense",
-		id: 0,
+		id: 1,
 	},
 	{
 		title: "HacktheNorth 2021: Prografit",
 		description:
 			"Using HTML, CSS, Javascript, I was responsible to build a chrome extension to send an alert every 20 minutes to users. Users have the option to opt-out from being reminded when the time is still going. The submitted data will then be transferred to the webpage, which was built by my other teammates that will display users' statistics over the week. I did this project during HackTheNorth 2021, the largest hackathon in North America.",
-		image: "/images/3.png",
+		image: "/images/0.png",
 		tags: ["HTML", "CSS", "Javascript"],
 		source: "https://github.com/northmen-hackers",
 		visit: "https://devpost.com/software/prografit",
-		id: 1,
-	},
-	{
-		title: "To-do list",
-		description:
-			"Using React.js, I built a simple to-do list application. In this application, users can add a new to-do item and remove items everytime users click the selected to-do list. This was built during a workshop in HackTheNortth 2021, Canada's biggest hackathon.",
-		image: "/images/4.png",
-		tags: ["React"],
-		source: "https://github.com/chrishadrian/todo-react",
-		visit: "https://github.com/chrishadrian/todo-react",
-		id: 2,
-	},
-	{
-		title: "Richelle Shop",
-		description:
-			"Using Wix, a cloud-based web development services, I developed a full-stack website for a shoe retail online shop. The website is called Richelle Shop, as it resonates its brand and originality, and it allows users to browse through store's inventories, starting from its picture, size, color, description, price, and payment method.",
-		image: "/images/2.png",
-		tags: ["Wix"],
-		source: "https://richelle.shop",
-		visit: "https://richelle.shop",
-		id: 3,
+		id: 0,
 	},
 ];
 
