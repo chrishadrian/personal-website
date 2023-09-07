469b8b9728890834302476934bed788153dc0012
Christopher Wong <chrishadrianwong@gmail.com>
2023-09-06 22:50:29 -0700

    Updated hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -29,10 +29,9 @@ const Hero = () => (
 				An independent and self-motivated penultimate computer science
 				student at the University of British Columbia with interest in
 				software development, AI, Fintech, E-commerce, and cloud
-				computing services. Learn more about me by clicking the button
-				below or browse through this website.
+				computing services.
 			</SectionText>
-			<Button onClick={handleDownload}>Learn More</Button>
+			<Button onClick={handleDownload}>My Résumé</Button>
 		</LeftSection>
 	</Section>
 );
