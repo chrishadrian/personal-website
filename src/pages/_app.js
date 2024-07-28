29e7c82ff1e4695a29bc889b3c86d7762750d949
chrishadrian <chrishadrianwong@gmail.com>
2024-07-27 17:27:03 -0700

    Added head title

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,16 @@
+import Head from 'next/head';
 import Theme from '../styles/theme';
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Theme>
-        <Component {...pageProps} />
-      </Theme>
-    </>
-  );
+	return (
+		<>
+			<Head>
+				<title>Christopher Hadrian Wong</title>
+				<meta name="description" content="Website created with ❤️" />
+			</Head>
+			<Theme>
+				<Component {...pageProps} />
+			</Theme>
+		</>
+	);
 }
- 
