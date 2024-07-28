ac77369dd7936284eb197c0f038c0bab62fb80da
chrishadrian <chrishadrianwong@gmail.com>
2024-07-28 10:47:09 -0700

    Updated project's description

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,60 +1,62 @@
 export const projects = [
-	{
-		title: "Recipe Buddy",
-		description:
-			"This web application was crafted with ReactJS, Auth0, and Firebase Firestore to deliver a streamlined solution for organizing online video recipes sourced from platforms like Instagram and TikTok. It allows users to gather recipes, access tutorial videos, ingredient lists, and instructions, while also offering a meal planning feature integrated with a calendar. Additionally, the application automatically generates shopping lists based on scheduled recipes, enhancing the overall cooking experience with efficiency and convenience.",
-		image: "/images/RecipeBuddy.png",
-		tags: ["ReactJS", "Auth0", "Firebase"],
-		source: "https://github.com/chrishadrian/my-recipe-buddy",
-		video: "https://www.youtube.com/",
-		visit: "https://myrecipebuddy.netlify.app/",
-		id: 4,
-	},
-	{
-		title: "GISAU Website",
-		description:
-			"This student association's web application serves as the central hub for the Indonesian student association at UBC, showcasing its online presence. Developed with ReactJS, Express, Typescript, Tailwind CSS, Auth0, Firebase Function, and Firebase Firestore, the application includes key pages such as home, about us, events, partners,  contact us, and games. It effectively highlights the association's mission and activities, demonstrating proficiency in modern web development technologies.",
-		image: "/images/GISAU.png",
-		tags: ["ReactJS", "Express", "Typescript", "Tailwind CSS", "Auth0", "Firebase"],
-		source: "https://github.com/gisauweb/GISAUBC",
-		video: "https://www.youtube.com/",
-		visit: "https://gisaubc.com",
-		id: 3,
-	},
-	{
-		title: "Sync-Up",
-		description:
-			"This web application, developed using ReactJS, ExpressJS, NodeJS, Firebase Authentication, and Firebase Firestore, is designed to enhance the study session scheduling experience. Users can effortlessly arrange study sessions with friends, earning reward points based on session duration, which can later be redeemed for sponsor coupons. Additionally, the application offers the flexibility to choose between in-person and virtual meeting options, catering to diverse user preferences.",
-		image: "/images/SyncUp.png",
-		tags: ["ReactJS", "Express", "NodeJS", "Firebase"],
-		source: "https://github.com/Julian-UBC/Sync-Up",
-		video: "https://www.youtube.com/",
-		visit: "https://github.com/Julian-UBC/Sync-Up",
-		id: 2,
-	},
-	{
-		title: "Unixpense",
-		description:
-			"A user-friendly expense tracker application built with Java, Java Swing, and JSON. This project included the creation of an end-to-end object-oriented program (OOP) in Java, seamless data read/write using JSON, and an intuitive graphical user interface (GUI) designed with Java Swing. Users can easily manage expenses, perform actions like creation, deletion, saving, loading, and in-depth analysis.",
-		image: "/images/1.png",
-		tags: ["Java", "JSON"],
-		source: "https://github.com/chrishadrian/Unixpense",
-		video: "https://www.youtube.com/",
-		visit: "https://github.com/chrishadrian/Unixpense",
-		id: 1,
-	},
-	{
-		title: "HacktheNorth 2021: Prografit",
-		description:
-			"A Chrome extension was developed using HTML, CSS, and JavaScript. This extension's primary function was to send periodic alerts to users every 20 minutes, with the option for users to opt out of reminders while the timer was active. User data collected by the extension was then transmitted to a dashboard, allowing users to access and analyze their weekly statistics.",
-		image: "/images/0.png",
-		tags: ["HTML", "CSS", "Javascript"],
-		source: "https://github.com/northmen-hackers",
-		video: "https://www.youtube.com/",
-		visit: "https://devpost.com/software/prografit",
-		id: 0,
-	},
+  {
+    title: "Recipe Buddy",
+    description:
+      "A web application for organizing online video recipes sourced from platforms like Instagram and TikTok. It allows users to gather recipes, access tutorial videos, ingredient lists, instructions, and offers a meal planning feature integrated with a calendar. The application automatically generates shopping lists based on scheduled recipes, enhancing the overall cooking experience with efficiency and convenience. Developed with ReactJS, Auth0, and Firebase Firestore.",
+    image: "/images/RecipeBuddy.png",
+    tags: ["ReactJS", "Auth0", "Firebase"],
+    source: "https://github.com/chrishadrian/my-recipe-buddy",
+    video: "https://www.youtube.com/",
+    visit: "https://myrecipebuddy.netlify.app/",
+    id: 4,
+  },
+  {
+    title: "GISAU Website",
+    description:
+      "A web application serving as the central hub for the Indonesian student association at UBC, showcasing its online presence. It includes key pages such as home, about us, events, partners, contact us, and games, effectively highlighting the association's mission and activities. Developed with ReactJS, Express, Typescript, Tailwind CSS, Auth0, Firebase Function, and Firebase Firestore.",
+    image: "/images/GISAU.png",
+    tags: ["ReactJS", "Express", "Typescript", "Tailwind CSS", "Auth0", "Firebase"],
+    source: "https://github.com/gisauweb/GISAUBC",
+    video: "https://www.youtube.com/",
+    visit: "https://gisaubc.com",
+    id: 3,
+  },
+  {
+    title: "Sync-Up",
+    description:
+      "A web application designed to enhance the study session scheduling experience. Users can effortlessly arrange study sessions with friends, earn reward points based on session duration, which can later be redeemed for sponsor coupons. The application offers flexibility to choose between in-person and virtual meeting options. Developed using ReactJS, ExpressJS, NodeJS, Firebase Authentication, and Firebase Firestore.",
+    image: "/images/SyncUp.png",
+    tags: ["ReactJS", "Express", "NodeJS", "Firebase"],
+    source: "https://github.com/Julian-UBC/Sync-Up",
+    video: "https://www.youtube.com/",
+    visit: "https://github.com/Julian-UBC/Sync-Up",
+    id: 2,
+  },
+  {
+    title: "Unixpense",
+    description:
+      "A user-friendly expense tracker application that allows users to manage expenses, perform actions like creation, deletion, saving, loading, and in-depth analysis. The project includes an end-to-end object-oriented program (OOP) in Java, seamless data read/write using JSON, and an intuitive graphical user interface (GUI) designed with Java Swing.",
+    image: "/images/1.png",
+    tags: ["Java", "JSON"],
+    source: "https://github.com/chrishadrian/Unixpense",
+    video: "https://www.youtube.com/",
+    visit: "https://github.com/chrishadrian/Unixpense",
+    id: 1,
+  },
+  {
+    title: "HacktheNorth 2021: Prografit",
+    description:
+      "A Chrome extension that sends periodic alerts to users every 20 minutes, with an option to opt out of reminders while the timer is active. User data collected by the extension is transmitted to a dashboard, allowing users to access and analyze their weekly statistics. Developed using HTML, CSS, and JavaScript.",
+    image: "/images/0.png",
+    tags: ["HTML", "CSS", "Javascript"],
+    source: "https://github.com/northmen-hackers",
+    video: "https://www.youtube.com/",
+    visit: "https://devpost.com/software/prografit",
+    id: 0,
+  },
 ];
+
+
 
 export const TimeLineData = [
 	{ year: 2020, text: "Admitted to The University of British Columbia" },
